48986ed86f9f31a9a9cec254765212d65c8353f0
edwardkoikai <edwardkoikai@gmail.com>
2024-08-02 23:36:46 +0300

    chore: add Flask migration files for database schema changes

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import os
 from models import db, Property, Rent, Purchase, Agent, User
 from flask_migrate import Migrate
-from flask import FLask
+from flask import Flask
 from flask_restful import Resource, Api
 from flask_cors import CORS
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
 
-app = FLask(__name__)
+app = Flask(__name__)
 api = Api(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
