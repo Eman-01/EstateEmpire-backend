b81e6e748badb4110e066facfe4b4e6b95d777e6
Emmanuel Joel Odhiambo <joel.emmanuel1968@gmail.com>
2024-08-07 14:48:07 +0300

    updated app.py

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from models import db
 from resources.agent import SignupResource, LoginResource
+from resources.purchase import PurchaseResource
+from resources.rental import RentalResource
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
@@ -16,19 +18,19 @@ app = Flask(__name__)
 api = Api(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["JWT_SECRET_KEY"] = "your_secret_key"  # Change this to a secure key
+app.config["JWT_SECRET_KEY"] = "your_secret_key"  
 app.json.compact = False
 
-# Setup CORS
+
 CORS(app)
 
-# Setup bcrypt
+
 bcrypt = Bcrypt(app)
 
-# Setup JWT
+
 jwt = JWTManager(app)
 
-# Setup database and migrations
+
 migrate = Migrate(app, db)
 db.init_app(app)
 
@@ -36,7 +38,8 @@ db.init_app(app)
 def index():
     return "<h1>EstateEmpire</h1>"
 
-# Add API resources
+api.add_resource(RentalResource, '/rentals', '/rentals/<int:id>')
+api.add_resource(PurchaseResource, '/purchases', '/purchases/<int:id>')
 api.add_resource(SignupResource, '/signup')
 api.add_resource(LoginResource, '/login')
 
