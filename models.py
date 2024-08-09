69acf1db93f00840a66ed564518352be9445fdf9
Emmanuel Joel Odhiambo <joel.emmanuel1968@gmail.com>
2024-08-09 18:17:19 +0300

    updated user table

--- models.py
+++ models.py
@@ -110,12 +110,14 @@ class User(db.Model, UserMixin):
     __tablename__ = 'users'
     
     id = db.Column(db.Integer, primary_key=True)
+    contact = db.Column(db.Integer, unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
-    properties = db.relationship('Property', back_populates='user')
-   
+    role = db.Column(db.String, nullable=False, default='client')
+    created_at = db.Column(db.DateTime, default=func.now())
     
-    rented = db.relationship('Rented', back_populates='user', lazy=True)
+    properties = db.relationship('Property', back_populates='user', lazy=True)
+    rented_properties = db.relationship('Rented', back_populates='user', lazy=True)
     purchases = db.relationship('Purchase', back_populates='user', lazy=True)
     
     def set_password(self, password):
@@ -128,4 +130,6 @@ class User(db.Model, UserMixin):
         return {
             "id": self.id,
             "email": self.email,
+            "role": self.role
         }
+
